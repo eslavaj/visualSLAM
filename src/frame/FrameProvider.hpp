75c07f4c109d4cea2c2c57bfa5c7613d3f7af7c3
Jose ESLAVA <joseslava@gmail.com>
2021-06-28 23:20:21 +0200

    chore: Frame provider documentation

--- src/frame/FrameProvider.hpp
+++ src/frame/FrameProvider.hpp
@@ -5,6 +5,13 @@
  *      Author: user3
  */
 
+/**
+ *
+ * @file FrameProvider.hpp
+ * @brief  Header file for frame provider class.
+ *
+ */
+
 #ifndef FRAME_FRAMEPROVIDER_HPP_
 #define FRAME_FRAMEPROVIDER_HPP_
 
@@ -16,22 +23,45 @@
 #include <mutex>
 
 
-
+/**
+ * Frame provider class, this class gets frames and serves them to other threads. It is useful to simulate a frame flow from a set of images
+ *
+ */
 class FrameProvider{
-/*TODO: need to implement end mechanism*/
+/*TODO: need to implement terminate mechanism*/
 public:
+	/**
+	 * @brief Frame provider constructor.
+	 *
+	 * @param mtx: mutex to control access to shared resources
+	 * @param frameAvail: to tell a new frame is available
+	 * @param frame: the shared frame
+	 *
+	 */
 	FrameProvider(std::mutex & mtx, bool & frameAvail, cv::Mat & frame):
 				  m_mutex(mtx), m_frameAvailable(frameAvail), m_frame(frame){};
 
+	/**
+	 * @brief retrieves images from a folder and serves them as a 30 fps flow.
+	 *
+	 * @param folderPath: folder containing the set of images
+	 * @param startIdx: index of the first image
+	 * @param endIdx: image of the last image
+	 *
+	 */
 	void captureFramesFromFolder(const std::string folderPath, size_t startIdx, size_t endIdx);
+
+	/**
+	 * @brief retrieves images from an argus camera and serves them as a 30 fps flow - IN PROGRESS.
+	 *
+	 */
 	void captureFramesFromArgusCam();
 
 
 private:
-	std::thread * m_captureFrameThread;
-	std::mutex & m_mutex;
-	bool & m_frameAvailable;
-	cv::Mat & m_frame;
+	std::mutex & m_mutex;									/*!< mutex to control resources access */
+	bool & m_frameAvailable;								/*!< to tell a frame is available */
+	cv::Mat & m_frame;										/*!< the frame */
 
 };
 
